82676d0972f7895ab0e8fcb609b02572df328405
gx1999 <aaron131402@gmail.com>
2020-06-01 15:18:47 +0800

    gx

--- app/src/main/java/gx/upc/edu/cn/Main6Activity.java
+++ app/src/main/java/gx/upc/edu/cn/Main6Activity.java
@@ -2,9 +2,11 @@ package gx.upc.edu.cn;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -25,19 +27,19 @@ import java.net.URL;
 public class Main6Activity extends AppCompatActivity {
 
     TextView t1;
-    private Handler handler=new Handler()
-    {
+    private Handler handler = new Handler() {
         @Override
-        public void handleMessage(Message msg)
-        {
-            String result=(String)msg.getData().get("result");
+        public void handleMessage(Message msg) {
+            String result = (String) msg.getData().get("result");
             t1.setText(result);
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+
         Button b1=(Button)findViewById(R.id.b1);
         t1=findViewById(R.id.t1);
         b1.setOnClickListener(new View.OnClickListener() {
@@ -55,8 +57,9 @@ public class Main6Activity extends AppCompatActivity {
            }
         }.start();
 
-    }
-    public void getData()
+
+        }
+      public void getData()
     {
         try {
             String path = "http://101.201.239.28:9080/grid/points/getSmilePoints";
@@ -70,10 +73,10 @@ public class Main6Activity extends AppCompatActivity {
                 BufferedReader bis=new BufferedReader(new InputStreamReader(is));
                 String line=null;
                 line=bis.readLine();
-                /*if(line!=null)
+                if(line!=null)
                 {
                     t1.setText(line);
-                }*/
+                }
                // JSONObject json= new JSONObject(line);
             //    String status=json.optString("status");
                 Message msg=new Message();
@@ -86,4 +89,4 @@ public class Main6Activity extends AppCompatActivity {
         catch (IOException e)
         {e.printStackTrace();}
     }
-}
+    }
